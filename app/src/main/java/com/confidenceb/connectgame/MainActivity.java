5087c41339e2e64e0de1675c40848e958f3a9059
Confidence-bassey <confidencebass2@gmail.com>
2022-04-01 13:56:48 +0100

    Added a gameState logic to stop game when someone wins

--- app/src/main/java/com/confidenceb/connectgame/MainActivity.java
+++ app/src/main/java/com/confidenceb/connectgame/MainActivity.java
@@ -11,35 +11,39 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
+    boolean gameState = true;
     int[] gameStatus = {2,2,2,2,2,2,2,2,2};
     int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{2,4,6},{0,4,8}};
 
-    public void dropInCounter(View view){
+    public void dropInCounter(View view) {
         ImageView counter = (ImageView) view;
         Log.i("Tag", counter.getTag().toString());
         int clickedTag = Integer.parseInt(counter.getTag().toString());
-        gameStatus[clickedTag] = activePlayer;
-        counter.setTranslationY(-1500);
-        if(activePlayer==0){
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer= 1;
-        }else{
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
-        }
-        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
-        for(int[] winningPosition:winningPositions) {
-            if (gameStatus[winningPosition[0]]==gameStatus[winningPosition[1]]&& gameStatus[winningPosition[1]]==gameStatus[winningPosition[2]]&& gameStatus[winningPosition[0]]!=2) {
-                String gameWinner = "";
-                if(activePlayer==1){
-                    gameWinner = "Yellow";
-                }else{
-                    gameWinner = "Red";
+        if (gameStatus[clickedTag] == 2 && gameState) {
+            gameStatus[clickedTag] = activePlayer;
+            counter.setTranslationY(-1500);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+            for (int[] winningPosition : winningPositions) {
+                if (gameStatus[winningPosition[0]] == gameStatus[winningPosition[1]] && gameStatus[winningPosition[1]] == gameStatus[winningPosition[2]] && gameStatus[winningPosition[0]] != 2) {
+                    String gameWinner = "";
+                    if (activePlayer == 1) {
+                        gameWinner = "Yellow";
+                    } else {
+                        gameWinner = "Red";
+                    }
+                    //Log.i("TAG", "gamewinner: called");
+                    gameState = false;
+                    Log.i("INFO", gameWinner + " has won");
+                    Toast.makeText(this, gameWinner + " has won", Toast.LENGTH_LONG).show();
+                    Log.i("TAG", "gamewinner: announced");
                 }
-                //Log.i("TAG", "gamewinner: called");
-                Log.i("INFO", gameWinner +" has won");
-                Toast.makeText(this, gameWinner+" has won", Toast.LENGTH_LONG).show();
-                Log.i("TAG", "gamewinner: announced");
             }
         }
     }
